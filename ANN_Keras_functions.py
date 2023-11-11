98a3182dfe1d75e2c36ee71624d07e54ce6982fb
kamilbanas85 <53495965+kamilbanas85@users.noreply.github.com>
2023-11-11 21:27:56 +0100

    Update ANN_Keras_functions.py

--- ANN_Keras_functions.py
+++ ANN_Keras_functions.py
@@ -14,6 +14,8 @@ from keras.layers import Dropout
 from keras.constraints import MaxNorm
 
 ##########################################################
+###########################################################
+###
 
 def PlotLossTrainVsVal(History):
     
@@ -25,6 +27,8 @@ def PlotLossTrainVsVal(History):
     plt.show()
 
 ###########################################################
+###########################################################
+### 
 
 def PlotAccuracyTrainVsVal(History):
     
@@ -34,8 +38,93 @@ def PlotAccuracyTrainVsVal(History):
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy score')
     plt.show()
+
+###########################################################
+###########################################################
+### 
+
+def create_feed_forward_model(hidden_layers_nr = 1,
+                              neurons_nr = 10,
+                              loss = 'mean_squared_error',
+                              optimizer = 'adam',
+                              input_shape = (1, ),
+                              output_nodes_nr = 1,
+                              add_batch_norm = False,
+                              activation_fun = 'relu',
+                              activation_out = 'linear',
+                              dropout = None,
+                              constraint_value = None,
+                              init = 'glorot_uniform',
+                              regression_type = True):
+
+    ##############################################
+    # init model
+    model = Sequential()
     
+    ##############################################
+    # 1 layer - imput layer
+    if constraint_value is not None:
+        model.add(Dense(neurons_nr, input_shape = input_shape,\
+                        activation = activation_fun,\
+                        kernel_initializer = init,\
+                        kernel_constraint = MaxNorm(constraint_value) ) )
+    else:
+        model.add(Dense(neurons_nr, input_shape = input_shape,\
+                        activation = activation_fun,\
+                        kernel_initializer = init ) )
+        
+    if dropout is not None:
+        model.add(Dropout( dropout ))
+    
+    
+    ##############################################
+    # mid layers
+    for ln in range(hidden_layers_nr-1):
+        
+        if constraint_value is not None:
+            model.add(Dense(neurons_nr,\
+                            activation = activation_fun,\
+                            kernel_initializer = init,\
+                            kernel_constraint = MaxNorm(constraint_value) ) )
+        else:
+            model.add(Dense(neurons_nr,\
+                            activation = activation_fun,\
+                            kernel_initializer = init ) )
+
+        if add_batch_norm:
+           model.add( BatchNormalization() )
+           
+        if dropout is not None:
+            model.add(Dropout( dropout ))
+	
+    
+    ##############################################
+    # output layer
+    model.add(Dense(output_nodes_nr, activation = activation_out,\
+                    kernel_initializer = init ))
+    
+    #Opt = eval(f'tf.optimizers.{Opt}')
+    #lr_metric = get_lr_metric(Opt)
+
+
+    ##############################################
+	# Compile model:
+    if regression_type == True:
+        model.compile(loss = loss, optimizer = optimizer, metrics=['mse', 'mae', 'mape'] )
+    else:
+        model.compile(loss = loss, optimizer = optimizer, metrics=["accuracy", "AUC"] )
+
+
+
+    return model
+
+
+
+
+
 ############################################################
+###########################################################
+### old-version
 
 def get_lr_metric(optimizer):
     def lr(y_true, y_pred):
@@ -100,3 +189,7 @@ def CreateFeedForwardModel(HiddenLayersNumber=1,\
     model.compile(loss = LossFun, optimizer=Opt, metrics=[LossFun, lr_metric] )
     
     return model
+
+
+
+
