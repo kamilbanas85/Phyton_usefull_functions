affaa97d8867fcc321ca96109540c19205602788
kamilbanas85 <53495965+kamilbanas85@users.noreply.github.com>
2023-11-24 00:17:27 +0100

    Update 04_Python_ML.py

--- 04_Python_ML.py
+++ 04_Python_ML.py
@@ -173,6 +173,127 @@ def table_grid_search(clf, all_columns=False, all_ranks=False, save=True):
     display(cv_results)
 
 ##################################################################################
+### 
+
+def plot_grid_search_few_score(clf, for_github = False, score_name = None):
+    """Plot as many graphs as parameters are in the grid search results.
+
+    Each graph has the values of each parameter in the X axis and the Score in the Y axis.
+
+    Parameters
+    ----------
+    clf: estimator object result of a GridSearchCV
+        This object contains all the information of the cross validated results for all the parameters combinations.
+    """
+    # Convert the cross validated results in a DataFrame ordered by `rank_test_score` and `mean_fit_time`.
+    # As it is frequent to have more than one combination with the same max score,
+    # the one with the least mean fit time SHALL appear first.
+    
+    rank_test_score_var = 'rank_test_score'
+    mean_test_score_var = 'mean_test_score'
+    if score_name is not None:
+        rank_test_score_var = f'rank_test_{score_name}'
+        mean_test_score_var = f'mean_test_{score_name}'
+
+    
+    cv_results = pd.DataFrame(clf.cv_results_).sort_values(by=[rank_test_score_var, 'mean_fit_time'])
+
+    # Get parameters
+    parameters=cv_results['params'][0].keys()
+
+    parms_to_remove_if_ann = ['model__regression_type', 'model__input_shape', 'model__activation_out', 'model__output_nodes_nr']
+    parameters = [ele for ele in parameters if ele not in parms_to_remove_if_ann]
+    
+    # Calculate the number of rows and columns necessary
+    rows = -(-len(parameters) // 2)
+    columns = min(len(parameters), 2)
+    # Create the subplot
+    fig = make_subplots(rows=rows, cols=columns)
+    # Initialize row and column indexes
+    row = 1
+    column = 1
+
+    # For each of the parameters
+    for parameter in parameters:
+
+        # As all the graphs have the same traces, and by default all traces are shown in the legend,
+        # the description appears multiple times. Then, only show legend of the first graph.
+        if row == 1 and column == 1:
+            show_legend = True
+        else:
+            show_legend = False
+
+        # Mean test score
+        mean_test_score = cv_results[cv_results[rank_test_score_var] != 1]
+        fig.add_trace(go.Scatter(
+            name='Mean test score',
+            x=mean_test_score['param_' + parameter],
+            y=mean_test_score[mean_test_score_var],
+            mode='markers',
+            marker=dict(size=mean_test_score['mean_fit_time'],
+                        color='SteelBlue',
+                        sizeref=2. * cv_results['mean_fit_time'].max() / (40. ** 2),
+                        sizemin=4,
+                        sizemode='area'),
+            text=mean_test_score['params'].apply(
+                lambda x: pprint.pformat(x, width=-1).replace('{', '').replace('}', '').replace('\n', '<br />')),
+            showlegend=show_legend),
+            row=row,
+            col=column)
+
+        # Best estimators
+        rank_1 = cv_results[cv_results[rank_test_score_var] == 1]
+        fig.add_trace(go.Scatter(
+            name='Best estimators',
+            x=rank_1['param_' + parameter],
+            y=rank_1[mean_test_score_var],
+            mode='markers',
+            marker=dict(size=rank_1['mean_fit_time'],
+                        color='Crimson',
+                        sizeref=2. * cv_results['mean_fit_time'].max() / (40. ** 2),
+                        sizemin=4,
+                        sizemode='area'),
+            text=rank_1['params'].apply(str),
+            showlegend=show_legend),
+            row=row,
+            col=column)
+
+        fig.update_xaxes(title_text=parameter, row=row, col=column)
+        fig.update_yaxes(title_text='Score', row=row, col=column)
+
+        # Check the linearity of the series
+        # Only for numeric series
+        if pd.to_numeric(cv_results['param_' + parameter], errors='coerce').notnull().all():
+            x_values = cv_results['param_' + parameter].sort_values().unique().tolist()
+            r = stats.linregress(x_values, range(0, len(x_values))).rvalue
+            # If not so linear, then represent the data as logarithmic
+            if r < 0.86:
+                fig.update_xaxes(type='log', row=row, col=column)
+
+        # Increment the row and column indexes
+        column += 1
+        if column > columns:
+            column = 1
+            row += 1
+
+            # Show first the best estimators
+    fig.update_layout(legend=dict(traceorder='reversed'),
+                      width=columns * 360 + 100,
+                      height=rows * 360,
+                      title='Best score: {:.6f} with {}'.format(cv_results[mean_test_score_var].iloc[0],
+                                                                str(cv_results['params'].iloc[0]).replace('{',
+                                                                                                          '').replace(
+                                                                    '}', '')),
+                      hovermode='closest',
+                      template='none')
+    
+    if for_github:
+        fig.show("svg")
+    else:
+        fig.show()
+
+
+##################################################################################
 ### Confustion Matrix Plot
 
 def plot_confusion_matrix(y_test, y_hat, model, class_names):
